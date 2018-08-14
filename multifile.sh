781e431c5e8b4bb3d1fee36bd841f6e67e5498bc
Rob Moore <robert.moore@bglgroup.co.uk>
2018-08-14 13:50:29 +0100

    Add s3 handling to multifile.sh
    Also fix name of the main script: it's changed since I
    added that, oops.

--- multifile.sh
+++ multifile.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
-# Process files in "in" and send them to "out", zipped
+# Process files in "in" and send them to "out", zipped.
+# Upload the resulting files to the S3_BUCKET and S3_PREFIX specified
 
-for f in $(ls -1 in); do OUT="out/$(uuidgen).json"; ./jsonify.py -i "in/$f" -o "$OUT" && gzip "$OUT"; done
+if [[ "$S3_BUCKET" == '' || "$S3_KEY" == '' ]]; then
+  upload_s3() {
+    aws s3 cp "$1" "s3://$S3_BUCKET/$2"
+  }
+else
+  echo 'No S3_BUCKET or S3_KEY env vars set so not uploading to s3'
+  upload_s3() {
+  }
+fi
+
+panic() {
+  echo 'Something went wrong!' >&1
+  exit 1
+}
+
+process_file() {
+  f="$1"
+  OUT="out/$(uuidgen).json"
+  ZIPPED="$OUT.gz"
+  S3_KEY="$S3_PREFIX/$(basename $OUT)"
+
+  ./csv_to_json.py "$f" "$OUT" || panic
+  gzip "$OUT" || panic
+  upload_s3 "$ZIPPED" "$S3_KEY" || panic
+}
+
+for f in $(ls -1 in); do process_file "$f" done
